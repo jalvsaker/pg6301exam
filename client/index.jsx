c4a5ebb8530996904800d9b98248efc10adbc020
jalvsaker <jalvsaker@users.noreply.github.com>
2022-11-21 10:35:33 +0100

    prettier

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+function App() {
+  return <h1>Hei verden</h1>;
+}
+
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello World!</h1>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
