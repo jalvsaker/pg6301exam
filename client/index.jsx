2a897a5aba6d7d5852f3901c0943dadd814164bc
jalvsaker <jalvsaker@users.noreply.github.com>
2022-11-21 10:44:38 +0100

    browser router

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Homepage } from "./homepage";
 
 function App() {
-  return <h1>Hei verden</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Homepage />} />
+        <Route path={"/test"} element={<h2>test</h2>} />
+        <Route path={"/*"} element={<Navigate to={"/"} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 const root = createRoot(document.getElementById("root"));
--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 
 app.use(express.static("../client/dist"));
 
-const port = process.env.PORT || 3000;
+app.use((req, res, next) => {
+  if (req.method === "GET") {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
