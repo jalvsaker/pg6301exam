d0d731f01ad0f2503313b7827fd04168ac739aa0
jalvsaker <jalvsaker@users.noreply.github.com>
2022-11-22 15:17:24 +0100

    test

--- server/__tests__/test.js
+++ server/__tests__/test.js
@@ -1,45 +1,40 @@
-/*
-import express from "express";
+/*import express from "express";
 import request from "supertest";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
-import { loginApi } from "../loginApi.js";
-
-const app = express();
-app.use(bodyParser.json);
-dotenv.config();
-const mongoClient = new MongoClient(process.env.MONGODB_URL);
-
-beforeAll(async () => {
-  await mongoClient.connect();
-  const database = mongoClient.db("unit_tests");
-  app.use("/api/login", loginApi(database));
-});
-
-afterAll(async () => {
-  await mongoClient.close();
-});
-
-describe("tests", () => {
-  it("should do something", async () => {
-    const agent = request.agent(app);
-    const response = await agent.get("/api/login");
-
-    expect(response.status).toEqual(200);
-  });
-});
-*/
-
-import { foodApi } from "../foodApi.js";
+import { MongoClient } from "mongodb";*/
 import { loginApi, loginMiddleware } from "../loginApi.js";
+import { foodApi } from "../foodApi.js";
 import { orderApi } from "../orderApi.js";
 
-describe("", function() {
-  it("should ", function() {
-    foodApi()
-    loginApi()
-    loginMiddleware()
-    orderApi()
+/*describe("tests", () => {
+  const app = express();
+  app.use(bodyParser.json);
+  dotenv.config();
+  const mongoClient = new MongoClient(process.env.MONGODB_URL);
+
+  beforeAll(async () => {
+    await mongoClient.connect();
+    const database = mongoClient.db("unit_tests");
+    app.use("/api/login", loginApi(database));
+  });
+
+  afterAll(async () => {
+    await mongoClient.close();
+  });
+
+  it("should do something", async () => {
+    const result = request(app).get("/api/login").expect(404).end();
+
+    console.log(result)
+  });
+});*/
+
+describe("", () => {
+  it("should ", () => {
+    foodApi();
+    loginApi();
+    loginMiddleware();
+    orderApi();
   });
 });
