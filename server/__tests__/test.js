aff3d4c3021e07a1cb4fca0e9f3e4104f371d4b2
jalvsaker <jalvsaker@users.noreply.github.com>
2022-11-22 13:07:09 +0100

    test

--- server/__tests__/test.js
+++ server/__tests__/test.js
@@ -1,3 +1,4 @@
+/*
 import express from "express";
 import request from "supertest";
 import bodyParser from "body-parser";
@@ -5,29 +6,27 @@ import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 import { loginApi } from "../loginApi.js";
 
-let app;
-let mongoClient;
+const app = express();
+app.use(bodyParser.json);
+dotenv.config();
+const mongoClient = new MongoClient(process.env.MONGODB_URL);
 
-/*beforeAll(async () => {
-  app = express();
-  app.use(bodyParser.json);
-  dotenv.config();
-  mongoClient = new MongoClient(process.env.MONGODB_URL);
+beforeAll(async () => {
   await mongoClient.connect();
   const database = mongoClient.db("unit_tests");
   app.use("/api/login", loginApi(database));
 });
 
-afterAll(() => {
-  mongoClient.close();
-  app.stop()
-});*/
+afterAll(async () => {
+  await mongoClient.close();
+});
 
 describe("tests", () => {
   it("should do something", async () => {
-    /*const agent = request.agent(app);
+    const agent = request.agent(app);
     const response = await agent.get("/api/login");
 
-    expect(response.status).toEqual(200)*/
+    expect(response.status).toEqual(200);
   });
 });
+*/
