9e0176ab4b318947703bc478359ff001048388ae
jalvsaker <jalvsaker@users.noreply.github.com>
2022-11-22 09:21:18 +0100

    trying to test server

--- server/__tests__/test.js
+++ server/__tests__/test.js
@@ -1,3 +1,33 @@
-describe("tests", function () {
-  it("should do something", function () {});
+import express from "express";
+import request from "supertest";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import { loginApi } from "../loginApi.js";
+
+let app;
+let mongoClient;
+
+/*beforeAll(async () => {
+  app = express();
+  app.use(bodyParser.json);
+  dotenv.config();
+  mongoClient = new MongoClient(process.env.MONGODB_URL);
+  await mongoClient.connect();
+  const database = mongoClient.db("unit_tests");
+  app.use("/api/login", loginApi(database));
+});
+
+afterAll(() => {
+  mongoClient.close();
+  app.stop()
+});*/
+
+describe("tests", () => {
+  it("should do something", async () => {
+    /*const agent = request.agent(app);
+    const response = await agent.get("/api/login");
+
+    expect(response.status).toEqual(200)*/
+  });
 });
